57f12f61359fb9ef25bb42194093d4c47065bf86
Martez Reed <martez.reed@greenreedtech.com>
2018-10-30 16:31:05 -0500

    Update tf-vault.tf

--- tf-vault.tf
+++ tf-vault.tf
@@ -1,5 +1,9 @@
 provider "vault" {}
 
-resource "vault_auth_backend" "example" {
+resource "vault_auth_backend" "github_example" {
   type = "github"
+}
+
+resource "vault_auth_backend" "aws_example" {
+  type = "aws"
 }
