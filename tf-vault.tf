98f099f744e3fb6f1b2d8daac1718219de87a97d
Martez Reed <martez.reed@greenreedtech.com>
2018-10-30 18:20:49 -0500

    Update tf-vault.tf

--- tf-vault.tf
+++ tf-vault.tf
@@ -27,5 +27,5 @@ resource "vault_database_secret_backend_role" "role" {
   backend             = "${vault_mount.db.path}"
   name                = "prod"
   db_name             = "${vault_database_secret_backend_connection.postgres.name}"
-  creation_statements = "CREATE ROLE {{name}} WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}';"
+  creation_statements = "CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}';"
 }
