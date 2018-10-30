31d7f2cdc70fd23b33b4cd853ed6cf4624cc200f
Martez Reed <martez.reed@greenreedtech.com>
2018-10-30 16:29:19 -0500

    Create vault-admin-policy.tf

---
+++ vault-admin-policy.tf
@@ -0,0 +1,45 @@
+resource "vault_policy" "example" {
+  name = "admin-policy"
+
+  policy = <<EOT
+path "auth/*"
+{
+  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
+
+path "sys/auth/*"
+{
+  capabilities = ["create", "read", "update", "delete", "sudo"]
+}
+
+path "sys/policy"
+{
+  capabilities = ["read"]
+}
+
+path "sys/policy/*"
+{
+  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
+
+path "secret/*"
+{
+  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
+
+path "sys/mounts/*"
+{
+  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
+
+path "sys/mounts"
+{
+  capabilities = ["read"]
+}
+
+path "sys/health"
+{
+  capabilities = ["read", "sudo"]
+}
+EOT
+}
